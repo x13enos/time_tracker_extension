87323d2ed0a4623da8310fbaf99b3b6d02c8db32
Andres Sild <X3enos@gmail.com>
2020-06-27 21:29:41 +0300

    TT-195: added special helper for rendering json partials (#11)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,18 @@ RSpec.describe Workspace, type: :model do
     it { should have_many(:time_locking_periods)  }
   end
 
+  describe "unapproved_periods" do
+    let!(:workspace) { create(:workspace) }
+    let!(:user) { create(:user, active_workspace: workspace) }
+    let!(:period_1) { create(:time_locking_period, workspace: workspace, user: user, approved: true, end_of_period: Date.today - 1.week) }
+    let!(:period_2) { create(:time_locking_period, workspace: workspace, user: user, approved: false, end_of_period: Date.today) }
+    let!(:period_3) { create(:time_locking_period, workspace: workspace, user: user, approved: false, end_of_period: Date.today - 2.days) }
+
+    it "should return periods for current workspace and which weren't approve" do
+      expect(user.unapproved_periods).to eq([period_3])
+    end
+  end
+
   describe "generate_telegram_token" do
     it "should add Telegram token to new user before creating" do
       user = build(:user, active_workspace: create(:workspace))
