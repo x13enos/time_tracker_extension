651f64dc7566c6d6801faf2ef8a19455c5e9130e
Andres Sild <x3enos@gmail.com>
2020-07-28 15:14:25 +0300

    TT-218: added logic of sending daily reports for admins

---
+++ app/services/time_tracker_extension/daily_reports_sender.rb
@@ -0,0 +1,39 @@
+module TimeTrackerExtension
+  class DailyReportsSender
+    class << self
+
+      def execute
+        # TODO: change this with checking which admins have notification setting about sending daily reports,
+        # select workspaces based on that info
+        Workspace.all.each do |workspace|
+          create_reports(workspace)
+        end
+      end
+
+      private
+
+      def create_reports(workspace)
+        report_data = {
+          workspace_name: workspace.name,
+          users_data: users_data(workspace)
+        }
+        workspace.users.admin.each do |admin|
+          ::UserNotifier.new(admin, :daily_report, { report_data: report_data }).perform
+        end
+      end
+
+      def users_data(workspace)
+        workspace.users.inject({}) do |data, user|
+          tasks = workspace.time_records.where(user_id: user.id, assigned_date: Date.yesterday)
+          data[user.id] = {
+            name: user.name,
+            tasks_count: tasks.size,
+            total_time: tasks.sum(:spent_time)
+          }
+          data
+        end
+      end
+
+    end
+  end
+end
---
+++ spec/services/time_tracker_extension/daily_reports_sender_spec.rb
@@ -0,0 +1,46 @@
+require "rails_helper"
+
+module TimeTrackerExtension
+  RSpec.describe DailyReportsSender do
+    let!(:workspace) { create(:workspace) }
+    let!(:project) { create(:project, workspace: workspace) }
+    let!(:admin) { create(:user, :admin) }
+    let!(:staff) { create(:user, :staff) }
+    let(:report_data) do
+      {
+        workspace_name: workspace.name,
+        users_data: {
+          admin.id => {
+            name: admin.name,
+            tasks_count: 2,
+            total_time: 3
+          },
+          staff.id => {
+            name: staff.name,
+            tasks_count: 1,
+            total_time: 0.5
+          }
+        }
+      }
+    end
+
+    it "should collect and send info to admins about users and their tasks for yesterday" do
+      travel_to Date.today.beginning_of_month
+
+      create(:time_record, assigned_date: Date.yesterday, spent_time: 2, user: admin, project: project)
+      create(:time_record, assigned_date: Date.yesterday, spent_time: 1, user: admin, project: project)
+      create(:time_record, assigned_date: Date.yesterday - 1.day, spent_time: 5, user: admin, project: project)
+      create(:time_record, assigned_date: Date.yesterday, spent_time: 2, user: admin)
+
+      create(:time_record, assigned_date: Date.yesterday, spent_time: 0.5, user: staff, project: project)
+      create(:time_record, assigned_date: Date.yesterday, spent_time: 2, user: admin)
+
+      workspace.users << [admin, staff]
+      allow(workspace).to receive_message_chain(:users, :admin) { [admin] }
+      expect(::UserNotifier).to receive(:new).with(admin, :daily_report, { report_data: report_data }) { double(perform: true) }
+      TimeTrackerExtension::DailyReportsSender.execute
+      travel_back
+    end
+
+  end
+end
