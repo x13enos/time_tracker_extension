4973c1072f1465b15104411252ba06fd08a1c85d
Andres Sild <x3enos@gmail.com>
2020-10-24 11:01:48 +0300

    TT-228: added question mark to the method, wrote some specs for user notifiers module

---
+++ spec/services/time_tracker_extension/user_notifier_spec.rb
@@ -0,0 +1,83 @@
+require "rails_helper"
+
+class DummyUserNotifier
+  include TimeTrackerExtension::UserNotifier
+
+  def initialize(notification_data)
+    @user = notification_data[:user]
+    @additional_data = notification_data[:additional_data]
+    @notification_type = notification_data[:notification_type]
+    @workspace_id = notification_data[:workspace_id]
+  end
+
+  private
+  attr_reader :user, :notification_type, :additional_data, :workspace_id
+
+  def notify_by_email
+  end
+end
+
+module TimeTrackerExtension
+  RSpec.describe UserNotifier do
+    let(:user) { create(:user) }
+
+    describe "private method - notifications" do
+      let!(:notifier) do
+        DummyUserNotifier.new({
+          user: user,
+          additional_data: {},
+          notification_type: :approve_period
+        })
+      end
+
+      it "should call method for sending email" do
+        expect(notifier).to receive(:notify_by_email)
+        notifier.send(:notifications)
+      end
+
+      it "should call method for sending telegram message" do
+        expect(notifier).to receive(:notify_by_telegram)
+        notifier.send(:notifications)
+      end
+    end
+
+    describe "private method - notify_by_telegram" do
+      let!(:notifier) do
+        DummyUserNotifier.new({
+          user: user,
+          additional_data: {},
+          notification_type: :approve_period
+        })
+      end
+
+      it "should not call telegram notifier in case of absense the telegram id for user" do
+        expect(TimeTrackerExtension::Notifiers::Telegram).to_not receive(:new)
+        notifier.send(:notify_by_telegram)
+      end
+
+      it "should not call telegram notifier if user have been disabled this notification in settings" do
+        allow(user).to receive(:telegram_id) { 1 }
+        allow(notifier).to receive(:notification_found_in_settings?).with('telegram') { false }
+        expect(TimeTrackerExtension::Notifiers::Telegram).to_not receive(:new)
+        notifier.send(:notify_by_telegram)
+      end
+
+      it "should build new telegram notifier" do
+        allow(user).to receive(:telegram_id) { 1 }
+        allow(notifier).to receive(:notification_found_in_settings?).with('telegram') { true }
+        expect(TimeTrackerExtension::Notifiers::Telegram).to receive(:new).with(user, {}) { double(send: true) }
+        notifier.send(:notify_by_telegram)
+      end
+
+      it "should send message according to the notification type" do
+        allow(user).to receive(:telegram_id) { 1 }
+        allow(notifier).to receive(:notification_found_in_settings?).with('telegram') { true }
+        telegram_notifier = double
+        allow(TimeTrackerExtension::Notifiers::Telegram).to receive(:new).with(user, {}) { telegram_notifier }
+        expect(telegram_notifier).to receive(:send).with(:approve_period)
+        notifier.send(:notify_by_telegram)
+      end
+    end
+    
+  end
+end
