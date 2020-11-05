ce6a9d007d9133018ddfced681bf1f6986dcac6d
Andres Sild <x3enos@gmail.com>
2020-11-05 22:21:13 +0200

    TT-155: destroy time locking rules/periods along with workspace

--- app/models/time_tracker_extension/user_extension.rb
+++ app/models/time_tracker_extension/user_extension.rb
@@ -2,7 +2,8 @@ module TimeTrackerExtension
   module UserExtension
     def self.included(klass)
       klass.class_eval do
-        has_many :time_locking_periods, class_name: '::TimeTrackerExtension::TimeLockingPeriod'
+        has_many :time_locking_periods,
+          class_name: '::TimeTrackerExtension::TimeLockingPeriod', dependent: :destroy
 
         def unapproved_periods
           time_locking_periods
