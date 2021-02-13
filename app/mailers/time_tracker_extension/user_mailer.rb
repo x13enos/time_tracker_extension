58ad243bba316a5f1d53dd992946ac1c3337beea
Andres Sild <x3enos@gmail.com>
2021-02-13 14:15:46 +0200

    TT-296: fixed bug with opening expired email of approving time report

--- app/mailers/time_tracker_extension/user_mailer.rb
+++ app/mailers/time_tracker_extension/user_mailer.rb
@@ -2,7 +2,7 @@ class TimeTrackerExtension::UserMailer < ApplicationMailer
   def approve_time_locking_period(user, period)
     @user = user
     @period = period
-    @token = TokenCryptService.encode(@user.email, 24.hours)
+    @token = TokenCryptService.encode(@user.email, 1.week)
     mail(to: @user.email, subject: I18n.t("mailers.subjects.approve_your_timereport"))
   end
 
